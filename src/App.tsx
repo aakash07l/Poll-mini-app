528ef4ab8f20811825c1cc0269866b36f7221c92
Aakash Dev <100058578+aakash07l@users.noreply.github.com>
2025-07-20 08:50:42 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Poll Lite Mini App</h1>
+    {/* Yahan poll component ki logic/JSX lagayen */}
+    <p>Create and participate in polls.</p>
+  </div>
+);
+
+export default App;
