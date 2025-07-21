fab98ced6415408529173055eabf5a8a8b0f639a
Aakash Dev <100058578+aakash07l@users.noreply.github.com>
2025-07-21 16:56:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WagmiProvider } from 'wagmi'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from './App.tsx'
+import { config } from './wagmi.ts'
+
+import './style.css'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
-);
+)
